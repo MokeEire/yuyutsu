b5dacdb4ad2e3c9d31618b6d78983570bda6e5ac
Mark B <markkbarrett@gmail.com>
2024-05-08 17:27:29 -0700

    Add curved text elements to map

--- src/routes/posts/dubois-routes/Map.svelte
+++ src/routes/posts/dubois-routes/Map.svelte
@@ -45,7 +45,6 @@
 
   import Globe from "./Globe.svelte";
   import Clip from "./Clip.svelte";
-
 </script>
  
   <div class="chart-container" bind:clientWidth={width}>
@@ -64,7 +63,44 @@
         stroke="black"
       clip-path={clip ? "url(#globe-shape)" : ""}
       />
-
+    <path
+      d="M {width * 0.25}, 0 A {width * 0.25} {height * 0.5} 0 0 1 {width *
+        0.5}, {height * 0.5}"
+      id="globe-text-left"
+      fill="none"
+      stroke="none"
+    />
+    <path
+      d="M {width * 0.5}, {height * 0.5} A {width * 0.25} {height *
+        0.5} 0 0 1 {width * 0.75}, 0"
+      id="globe-text-right"
+      fill="none"
+      stroke="none"
+    />
+    <text
+      dy="-6"
+      text-transform="upper"
+      opacity=".7"
+      font-weight="100"
+      font-size="{width / 900}rem"
+      clip-path={clip ? "" : "url(#globe-shape)"}
+    >
+      <textPath href="#globe-text-left" startOffset="27%" textLength="50%">
+        Distribution of
+      </textPath>
+    </text>
+    <text
+      dy="-6"
+      text-transform="upper"
+      opacity=".7"
+      font-weight="100"
+      font-size="{width / 900}rem"
+      clip-path={clip ? "" : "url(#globe-shape)"}
+    >
+      <textPath href="#globe-text-right" startOffset="35%" textLength="50%">
+        the Negro Race
+      </textPath>
+    </text>
       <!-- Countries -->
       {#each countries as country}
         <!-- svelte-ignore a11y-click-events-have-key-events --->
